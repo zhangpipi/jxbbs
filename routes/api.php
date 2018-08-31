9cd0d4e9f6bc94d0084cbc48a5000879f8afad0e
zhangbaiyu <zhangbaiyu@gmail.com>
2018-08-31 16:39:28 +0800

    可以通过手机注册

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,12 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
+
     // 短信验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')
         ->name('api.verificationCodes.store');
-});
 
-$api->version('v2', function($api) {
-    $api->get('version', function() {
-        return response('this is version v2');
-    });
+    // 用户注册
+    $api->post('users', 'UsersController@store')
+        ->name('api.users.store');
 });
